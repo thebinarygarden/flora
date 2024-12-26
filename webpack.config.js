190ddbf9075359dd7f12454eb3d0be314c659259
eduardo <thebinarygardener@gmail.com>
2024-12-25 18:42:44 -0600

    clean up config to be typescript specific

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,29 @@ module.exports = {
     mode: 'production',
     entry: './src/index.ts',  // Entry point of your library
     output: {
-        globalObject: 'typeof self !== "undefined" ? self : this',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        libraryTarget: 'umd',  // Important for library to be usable in various environments
-        library: 'flora'
+        filename: 'index.js', // CommonJS build
+        libraryTarget: 'umd',  // UMD module, usable in different environments
+        library: 'flora',
+        globalObject: 'typeof self !== "undefined" ? self : this', // Ensures compatibility in different environments
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.jsx?$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
+            },
+        ],
     },
     externals: {
         react: 'react',  // Assumes React is available as a peer dependency
         'react-dom': 'react-dom',
-        'framer-motion': 'framer-motion'
+        'framer-motion': 'framer-motion',
+        'styled-components': 'styled-components',  // Externalize styled-components
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx']
-    }
+        extensions: ['.tsx', '.ts'],
+    },
+    devtool: 'source-map',  // Helpful for debugging
 };

862db59ee2b04a82a41fb9cf2b21af449f77436a
eduardo <thebinarygardener@gmail.com>
2025-01-03 12:17:34 -0600

    integrating video looper

--- src/components/BGLanding/BGLanding.tsx
+++ src/components/BGLanding/BGLanding.tsx
@@ -2,12 +2,13 @@
 import React, {useState} from "react";
 import {motion} from "motion/react";
 import {BGLandingProps} from "../../types";
-import {ButtonContainer, FullPageMinimum, LandingTitle} from "./styled";
+import {ToggleVisButtonMotion, FullPageMinimum, LandingTitleMotion, ButtonContainerMotion} from "./styled";
 import {useAnimatedFields} from "./useAnimatedFields";
 import {LoremIpsum} from "../util/LoremIpsum";
 import {ClientOnlyFadeIn} from "../util/ClientOnlyFadeIn";
 import {Icon} from "../Icon/Icon";
 import {useFloraTheme} from "../../FloraThemeProvider";
+import {useVideoLooper} from "./useVideoLooper";
 
 export const BGLanding = ({children, title, imagePath, imageAlt, youtube, bgdocs, github}: BGLandingProps) => {
     const {
@@ -19,6 +20,7 @@ export const BGLanding = ({children, title, imagePath, imageAlt, youtube, bgdocs
         buttonsOpacity,
         contentY
     } = useAnimatedFields();
+    const {videoRef, isLooping, handleToggle} = useVideoLooper();
     const theme = useFloraTheme();
     const [titleShadowCustom, setTitleShadowCustom] = useState<string>();
 
@@ -27,33 +29,46 @@ export const BGLanding = ({children, title, imagePath, imageAlt, youtube, bgdocs
             setTitleShadowCustom("none");
             return;
         }
-        setTitleShadowCustom(`${value/3}px ${value/3}px ${value}px ${theme.primary}`)
+        setTitleShadowCustom(`${value / 3}px ${value / 3}px ${value}px ${theme.primary}`)
     })
 
     return (
         <ClientOnlyFadeIn>
-            <motion.img style={{
-                position: "fixed",
-                width: "100vw",
-                height: "100vh",
-                objectFit: "cover",
-                objectPosition: "center",
-                zIndex: -1,
-                y: thumbnailY,
-                opacity: thumbnailOpacity
-            }} src={imagePath} alt={imageAlt ?? ""}/>
-            <ButtonContainer style={{
-                opacity: buttonsOpacity
-            }}>
-                <a href={youtube}><Icon name="youtube"/></a>
-                <a href={youtube}><Icon name="github"/></a>
-                <a href={youtube}><Icon name="bgdocs"/></a>
-            </ButtonContainer>
-            <LandingTitle style={{
-                top: titleTop,
-                opacity: titleOpacity,
-                textShadow: titleShadowCustom
-            }}>{title}</LandingTitle>
+            <motion.video
+                ref={videoRef}
+                style={{
+                    position: "fixed",
+                    width: "100vw",
+                    height: "100vh",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                    zIndex: -1,
+                    y: thumbnailY,
+                    opacity: thumbnailOpacity
+                }} src={imagePath} muted/>
+            {!isLooping && (
+                <>
+                    <LandingTitleMotion style={{
+                        top: titleTop,
+                        opacity: titleOpacity,
+                        textShadow: titleShadowCustom
+                    }}>{title}</LandingTitleMotion>
+                    <ButtonContainerMotion style={{
+                        opacity: buttonsOpacity
+                    }}>
+                        <a href={youtube}><Icon name="youtube"/></a>
+                        <a href={github}><Icon name="github"/></a>
+                        <a href={bgdocs}><Icon name="bgdocs"/></a>
+                    </ButtonContainerMotion>
+                </>
+            )}
+            <ToggleVisButtonMotion
+                onClick={handleToggle}
+                style={{
+                    opacity: buttonsOpacity
+                }}>
+                {isLooping ? <Icon name="eyeoff"/> : <Icon name="eyeon"/>}
+            </ToggleVisButtonMotion>
             <motion.div style={{y: contentY}}>
                 <FullPageMinimum>
                     {children}

5011bbbecca1c26355d16f0ffe0123d0715b14bf
eduardo <thebinarygardener@gmail.com>
2025-08-10 13:00:19 -0500

    [checkpoint] optimization

--- packages/ui/src/core/BGLanding/useAnimatedFields.ts
+++ packages/ui/src/core/BGLanding/useAnimatedFields.ts
@@ -1,5 +1,4 @@
 import {useScroll, useTransform} from "framer-motion";
-import {useState} from "react";
 import {AnimatedFieldsProps} from "./types";
 
 export const useAnimatedFields = ({viewportHeight}: AnimatedFieldsProps) => {
@@ -8,24 +7,19 @@ export const useAnimatedFields = ({viewportHeight}: AnimatedFieldsProps) => {
 
     const thumbnailY = useTransform(scrollY, [0, phase[4]], [0, -100]);
     const thumbnailOpacity = useTransform(scrollY, [0, phase[2]], [1, 0]);
-    
-    const titleShadow = useTransform(scrollY, [phase[0], phase[2]], [10, 0]);
-    const [titleShadowCustom, setTitleShadowCustom] = useState<string>();
-    titleShadow.on('change', (value) => {
-        if (value === 0) {
-            setTitleShadowCustom("none");
-            return;
-        }
-        setTitleShadowCustom(`${value / 3}px ${value / 3}px ${value}px rgb(2 2 2)`) // near black
-    })
-    const titleTop = useTransform(scrollY, [phase[1], phase[3]], ['50%', '90%']);
+    const titleShadow = useTransform(
+        scrollY, 
+        [phase[0], phase[2]],
+        ["3.5px 3.5px 10px rgb(2 2 2)", "10px 10px 30px rgb(254 254 254)"]
+    );
+    const titleTop = useTransform(scrollY, [phase[0], phase[3]], ['50%', '10%']);
     const titleOpacity = useTransform(scrollY, [phase[2], phase[3]], [1, 0]);
     const buttonsOpacity = useTransform(scrollY, [phase[0], phase[1]], [1, 0]);
 
     return {
         thumbnailY,
         thumbnailOpacity,
-        titleShadow: titleShadowCustom,
+        titleShadow,
         titleTop,
         titleOpacity,
         buttonsOpacity

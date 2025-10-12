29440dd5c689946ab007060e2c1e1d8ad557ae93
eduardo <thebinarygardener@gmail.com>
2025-10-11 22:33:20 -0500

    CICD 1

--- packages/site/next.config.ts
+++ packages/site/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  output: "standalone"
 };
 
 export default nextConfig;
